da03625f1a2ce0e3de4941c34e945a9e5b910a3b
Mamady Sillah <supurpisces310@msn.com>
2020-02-10 21:46:22 -0500

    rough draft portfolio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch} from "react-router-dom";
-// import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+// import { BrowserRouter as Router, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./Home"
 import Nav from "../src/Nav"
+import About from "./About"
 function App() {
   return (
     <Router>
     <div className="App">
       <Nav />
       <Switch>
-        {/* <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home}/>
         <Route path="/About" exact component={About}/>
-        <Route path="/Projects" exact component={Projects}/>
+        {/* <Route path="/Projects" exact component={Projects}/>
         <Route path="/Contact" exact component={Contact}/> */}
         
       </Switch>
--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import "../src/Nav.css"
 
 
 
